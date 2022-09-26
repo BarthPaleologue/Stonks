5fa85a0ce1d14c3a87cd7446dc221ec3261fad25
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-09-26 19:37:19 +0200

    Invert button + shitpost

--- app/src/main/java/com/example/stonks/MainActivity.kt
+++ app/src/main/java/com/example/stonks/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.stonks
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -16,50 +18,66 @@ import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private var currentRate = 1.0
-    private var toCurrency = ""
-    private var fromCurrency = ""
+    private var toCurrency = "€"
+    private var fromCurrency = "€"
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<EditText>(R.id.startingAmount).addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if (getStartingAmount() > 1000000) rireDeDroite()
+            }
+
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                findViewById<TextView>(R.id.textView).text = getConvertedString(currentRate)
+                findViewById<TextView>(R.id.toCurrency).text =
+                    getConvertedString(currentRate) + toCurrency
             }
         })
 
-        // update every second
-        /*Thread {
-            var theta = 0.0
-            while (true) {
-                theta += 0.5
-                Thread.sleep(16)
-                runOnUiThread {
-                    findViewById<TextView>(R.id.convertButton).rotationY = theta.toFloat()
-                }
-            }
-        }.start()*/
+        findViewById<TextView>(R.id.fromCurrency).text = fromCurrency
+        findViewById<TextView>(R.id.toCurrency).text = "0$toCurrency"
+
+        money()
     }
 
-    fun isNumber(s: String?): Boolean {
+    private fun rireDeDroite() {
+        val resId = getResources().getIdentifier(
+            R.raw.rire_de_droite.toString(),
+            "raw", getPackageName()
+        )
+        val mediaPlayer = MediaPlayer.create(this, resId)
+        mediaPlayer.start()
+    }
+
+    private fun money() {
+        val resId = getResources().getIdentifier(
+            R.raw.money.toString(),
+            "raw", getPackageName()
+        )
+        val mediaPlayer = MediaPlayer.create(this, resId)
+        mediaPlayer.start()
+    }
+
+    private fun isNumber(s: String?): Boolean {
         return if (s.isNullOrEmpty()) false else s.all { Character.isDigit(it) || it == '.' }
     }
 
-    fun getStartingAmount(): Double {
-        val amountStr = findViewById<EditText>(R.id.startingAmount).text.toString();
-        amountStr.replace(Regex("[^€$£]"), "")
-        if(amountStr.isEmpty() || !isNumber(amountStr)) return 0.0
+    private fun getStartingAmount(): Double {
+        val amountStr = findViewById<EditText>(R.id.startingAmount).text.toString()
+        if (amountStr.isEmpty() || !isNumber(amountStr)) return 0.0
         return amountStr.toDouble()
     }
 
     fun getConvertedString(rate: Double): String {
-        return DecimalFormat("#.##").format(getStartingAmount() * rate) + toCurrency
+        return DecimalFormat("#.##").format(getStartingAmount() * rate).replace(",", ".")
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         // get rate from result
@@ -68,19 +86,38 @@ class MainActivity : AppCompatActivity() {
             toCurrency = data.getStringExtra("toCurrency") ?: ""
             fromCurrency = data.getStringExtra("fromCurrency") ?: ""
             findViewById<TextView>(R.id.fromCurrency).text = fromCurrency
-            findViewById<TextView>(R.id.textView).text = getConvertedString(currentRate)
+            findViewById<TextView>(R.id.toCurrency).text =
+                getConvertedString(currentRate) + toCurrency
         }
     }
 
+    @SuppressLint("SetTextI18n")
+    fun invert(view: View) {
+        val temp = fromCurrency
+        fromCurrency = toCurrency
+        toCurrency = temp
+
+        findViewById<EditText>(R.id.startingAmount).setText(getConvertedString(currentRate))
+        currentRate = 1 / currentRate
+
+        findViewById<TextView>(R.id.fromCurrency).text = fromCurrency
+        findViewById<TextView>(R.id.toCurrency).text =
+            "${getConvertedString(currentRate)}$toCurrency"
+    }
+
     fun chooseRate(view: View) {
-        val intent = Intent(this, RateActivity::class.java);
-        this.startActivityForResult(intent, 666);
+        val intent = Intent(this, RateActivity::class.java)
+        this.startActivityForResult(intent, 666)
     }
 
     fun copyResult(view: View) {
         val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText("label", getConvertedString(currentRate))
+        val clip = ClipData.newPlainText(
+            "label",
+            "${getStartingAmount().toString()}$fromCurrency is ${getConvertedString(currentRate)}$toCurrency"
+        )
         clipboard.setPrimaryClip(clip)
         Toast.makeText(this, "Copied to clipboard!", Toast.LENGTH_SHORT).show()
+        money()
     }
 }
